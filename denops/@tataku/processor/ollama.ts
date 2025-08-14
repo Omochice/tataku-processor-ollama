49a5411dfb984911bd562a392408b28829f0e180
Omochice <mochice.mls.ntl@gmail.com>
2025-08-14 10:20:40 +0900

    chore: upgrade ollama.js to 0.5.17
    It supports thinking levels

--- denops/@tataku/processor/ollama.ts
+++ denops/@tataku/processor/ollama.ts
@@ -3,7 +3,7 @@ import { as, ensure, is, type Predicate } from "jsr:@core/unknownutil@4.3.0";
 import { echo } from "jsr:@denops/std@7.6.0/helper/echo";
 import { toTransformStream } from "jsr:@std/streams@1.0.11/to-transform-stream";
 import { Ollama } from "npm:ollama@0.5.17/browser";
-import type { Message } from "npm:ollama@0.5.17/interfaces";
+import type { Message } from "npm:ollama@0.5.17";
 import { ProcessorFactory } from "jsr:@omochice/tataku-vim@1.2.1";
 
 type Option = {
